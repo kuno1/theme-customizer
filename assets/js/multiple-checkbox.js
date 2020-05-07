cf49d80f0c6de63621d70944f2fcd34e59195c42
fumikito <guy@hametuha.com>
2020-05-08 03:23:01 +0900

    Update multiple selector

--- assets/js/multiple-checkbox.js
+++ assets/js/multiple-checkbox.js
@@ -5,14 +5,12 @@
 /* global jQuery:false */
 
 jQuery( document ).ready( function( $ ) {
-	$( '.customize-control-checkbox-multiple input[type="checkbox"]' ).on(
-		'change',
-		function() {
-			var checkbox_values = $( this ).parents( '.customize-control' ).find( 'input[type="checkbox"]:checked' ).map( function() {
-				return this.value;
-			} ).get().join( ',' );
 
-			$( this ).parents( '.customize-control' ).find( 'input[type="hidden"]' ).val( checkbox_values ).trigger( 'change' );
-		}
-	);
+	$( '.kunoichi-customizer-multiple-checkbox input[type="checkbox"]' ).click( function () {
+		var checkbox_values = $( this ).parents( '.customize-control' ).find( 'input[type="checkbox"]:checked' ).map( function () {
+			return this.value;
+		} ).get().join( ',' );
+
+		$( this ).parents( '.customize-control' ).find( 'input[type="hidden"]' ).val( checkbox_values ).trigger( 'change' );
+	} );
 } );
--- src/Kunoichi/ThemeCustomizer/Controller/MultipleCheckbox.php
+++ src/Kunoichi/ThemeCustomizer/Controller/MultipleCheckbox.php
@@ -6,16 +6,16 @@ namespace Kunoichi\ThemeCustomizer\Controller;
 use Hametuha\StringUtility\Path;
 
 class MultipleCheckbox extends \WP_Customize_Control {
-	
+
 	use Path;
-	
+
 	/**
 	 * The type of customize control being rendered.
 	 *
 	 * @var    string
 	 */
 	public $type = 'multiple-checkbox';
-	
+
 	/**
 	 * Enqueue scripts/styles.
 	 */
@@ -23,7 +23,7 @@ class MultipleCheckbox extends \WP_Customize_Control {
 		$path = dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) . '/assets/js/multiple-checkbox.js';
 		wp_enqueue_script( 'theme-customizer-multiple-checkbox', $this->path_to_url( $path ), [ 'jquery' ], filemtime( $path ), true );
 	}
-	
+
 	/**
 	 * Displays the control content.
 	 */
@@ -32,18 +32,18 @@ class MultipleCheckbox extends \WP_Customize_Control {
 			return;
 		}
 		?>
-		
+
 		<?php if ( ! empty( $this->label ) ) : ?>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
 		<?php endif; ?>
-		
+
 		<?php if ( !empty( $this->description ) ) : ?>
 			<span class="description customize-control-description"><?php echo $this->description; ?></span>
 		<?php endif; ?>
-		
+
 		<?php $multi_values = ! is_array( $this->value() ) ? explode( ',', $this->value() ) : $this->value(); ?>
-		
-		<ul>
+
+		<ul class="kunoichi-customizer-multiple-checkbox">
 			<?php foreach ( $this->choices as $value => $label ) : ?>
 				<li>
 					<label>
@@ -51,10 +51,10 @@ class MultipleCheckbox extends \WP_Customize_Control {
 						<?php echo esc_html( $label ); ?>
 					</label>
 				</li>
-			
+
 			<?php endforeach; ?>
 		</ul>
-		
+
 		<input type="hidden" <?php $this->link(); ?> value="<?php echo esc_attr( implode( ',', $multi_values ) ); ?>" />
 		<?php
 	}
